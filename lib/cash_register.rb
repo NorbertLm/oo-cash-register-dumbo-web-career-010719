23fd974b73f40177de2163dd1c6397a6fd416733
Norbert Lam <norbertl@buffalo.edu>
2019-01-09 22:17:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   
   def apply_discount
     if self.discount != 0
-      self.total *= 100 - discount.fdiv(100)
+      self.total *= 10 - discount.fdiv(100)
     end
   end
   
